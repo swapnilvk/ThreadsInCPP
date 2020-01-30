11d6e51fe37cc51916cce9e36b063ebd21bcb764
Swapnil Kulkarni <kulkarni.swapnil.v@gmail.com>
2020-01-30 20:49:58 +0530

    program for Join threads

---
+++ ThreadJoin.cpp
@@ -0,0 +1,31 @@
+/* JOIN NOTES
+0. Once a thread is started we wait for this thread to finish by calling join() function on thread object.
+1. Double join will result into program termination.
+2. If needed we should check thread is joinable before joining. ( using joinable() function) */
+
+#include<iostream>
+#include<thread>
+#include<chrono>
+
+using namespace std;
+
+void PrintNumbers(int x)
+{
+    while(x-->0)
+        cout<<"Swapnil "<<x<<endl;
+
+    std::this_thread::sleep_for(chrono::seconds(5));
+}
+
+int main()
+{
+    std::thread t1(PrintNumbers, 10);
+    cout<<"main thread"<<endl;
+    
+    if(t1.joinable())
+        t1.join();
+
+cout<<"main after"<<endl;
+
+    return 0;
+}
