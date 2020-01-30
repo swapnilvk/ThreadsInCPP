7fc55aa29fdd952d7e543af35dc0e03875e399f0
Swapnil Kulkarni <kulkarni.swapnil.v@gmail.com>
2020-01-30 10:52:39 +0530

    Thread creation usning member function

---
+++ ThreadCreation/MemberFuction.cpp
@@ -0,0 +1,26 @@
+/*Thread creation using non static member function of class*/
+
+#include<iostream>
+#include<thread>
+
+
+class Base
+{
+public:
+    void PrintNumbers(int x)
+    {
+        while(x-->0)
+            std::cout<<x<<std::endl;
+    }
+};
+
+int main()
+{
+    Base b;
+    std::thread t1(&Base::PrintNumbers,&b,10);
+
+    t1.join();
+    return 0;
+}
+
+/* Compile:g++ -std=c++11 Functor.cpp -o Functor.o -pthread*/
